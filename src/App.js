c0bdc0fc9cd32b93e9c5298f2d3e3007778224ab
heejinsung <shj683706@gmail.com>
2022-02-03 23:21:07 +0900

    이벤트-state변경,bind함수,컴포넌트만들기

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ import './App.css';
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = { // state 값 초기화
-      // subject를 state화 시킴
+    this.state = { 
+      mode:'read',
+      selected_content_id:2,
       subject:{title:'WEB', sub:'World Wide Web!'},
+      welcome:{title:'Welcome', desc:'Hello, React!!'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is for information'},
         {id:2, title:'CSS', desc:'CSS is for design'},
@@ -19,16 +21,47 @@ class App extends Component {
     }
   }
   render() {
+    console.log('App render');
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read') {
+      var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i];
+        if (data.id === this.state.selected_content_id){
+          _title = data.title;
+          _desc = data.desc;
+          break; // 강제로 while문을 끝내는 코드
+        }
+        i = i + 1;
+      }
+      
+    }
+
+    console.log('render', this);
+
     return (
       <div className="App">
-        <Subject 
-          title={this.state.subject.title} 
-          sub={this.state.subject.sub}>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+          onChangePage={function () {
+            this.setState({ mode: 'welcome' });
+          }.bind(this)}
+        >
         </Subject>
-        <TOC data={this.state.contents}></TOC>
-        <Content
-          title="HTML"
-          desc="HTML is HyperText Markup Language."></Content>
+        <TOC 
+          onChangePage={function(id){
+            this.setState({
+              mode:'read',
+              selected_content_id:Number(id)
+            });
+          }.bind(this)} 
+          data={this.state.contents}
+        ></TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 
 class TOC extends Component {
   render() {
+    console.log('TOC render');
     var lists = [];
     var data = this.props.data;
     var i = 0;
     while(i < data.length){
       lists.push(
-        <li key={data[i].id}>
-          <a href={"/content/"+data[i].id}>{data[i].title}</a>
+        <li key={data[i].id}> 
+          <a 
+            href={"/content/"+data[i].id}
+            // 속성을 이용하지않고 하는 방법
+            //data-id={data[i].id}
+            onClick={function(id, e){
+              e.preventDefault();
+              this.props.onChangePage(id);
+            }.bind(this, data[i].id)}
+          >{data[i].title}</a>
         </li>
       );
       i= i + 1;
