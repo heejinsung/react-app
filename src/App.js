7d1e8c8c40e8490381d4a4f91a73ca89ecac2f50
heejinsung <shj683706@gmail.com>
2022-02-17 17:29:12 +0900

    delete 구현

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
     super(props);
     this.max_content_id = 3;
     this.state = { 
-      mode:'create',
+      mode:'welcome',
       selected_content_id:2,
       subject:{title:'WEB', sub:'World Wide Web!'},
       welcome:{title:'Welcome', desc:'Hello, React!!'},
@@ -54,18 +54,17 @@ class App extends Component {
       _article = (
         <CreateContent
           onSubmit={function (_title, _desc) {
-            // add content to this.state.contents
             this.max_content_id = this.max_content_id + 1;
-            // this.state.contents.push(
-            //   {id:this.max_content_id, title:_title, desc:_desc}
-            // );
-            var _contents = this.state.contents.concat({
+            var _contents = Array.from(this.state.contents);
+            _contents.push({
               id: this.max_content_id,
               title: _title,
               desc: _desc,
             });
             this.setState({
               contents: _contents,
+              mode:'read',
+              selected_content_id:this.max_content_id
             });
             console.log(_title, _desc);
           }.bind(this)}></CreateContent>
@@ -75,7 +74,7 @@ class App extends Component {
       _article = (
         <UpdateContent
           data={_content}
-          onSubmit={function (_id,_title, _desc) {
+          onSubmit={function (_id, _title, _desc) {
             var _contents = Array.from(this.state.contents);
             var i = 0;
             while(i < _contents.length){
@@ -87,6 +86,7 @@ class App extends Component {
             }
             this.setState({
               contents: _contents,
+              mode:'read'
             });
             console.log(_title, _desc);
           }.bind(this)}></UpdateContent>
@@ -117,9 +117,29 @@ class App extends Component {
           data={this.state.contents}
         ></TOC>
        <Control onChangeMode={function(_mode){
-         this.setState({
-           mode:_mode
-         })
+         if(_mode === 'delete'){
+          if(window.confirm('really??')){
+            var _contents = Array.from(this.state.contents);
+            var i = 0;
+            while(i < _contents.length){
+              if(_contents[i].id === this.state.selected_content_id){
+                _contents.splice(i,1);
+                break;
+              }
+              i = i + 1;
+            }
+            this.setState({
+              mode:'welcome',
+              contents:_contents
+            });
+            alert('deleted!!')
+          }
+         } else {
+          this.setState({
+            mode: _mode,
+          });
+         }
+         
        }.bind(this)}></Control>
         {this.getContent()}
       </div>
